3564283c76dbac716e90861ace44479ef5c60398
Michel <mschocken@gmail.com>
2023-05-16 12:28:40 -0400

    encadenar

--- Opcional/usuario.py
+++ Opcional/usuario.py
@@ -6,18 +6,22 @@ class Usuario:
 
     def hacer_deposito(self, amount):
         self.amount += amount
+        return self
 
     def hacer_retiro(self, amount):
         self.amount -= amount
+        return self
 
     def mostrar_balance_usuario(self):
         print(f"Usuario: {self.name}, Balance: {self.amount}")
+        return self
 
-    def transferir_dinero(self, amount, usuario):
+    def transferir_dinero(self, amount, usuario): #bonus
         self.amount -= amount
         usuario.amount += amount
         self.mostrar_balance_usuario()
         usuario.mostrar_balance_usuario()
+        return self
 
 
 michael = Usuario ("michael")
@@ -25,22 +29,10 @@ jenny = Usuario ("jenny")
 mario = Usuario ("mario")
 
 
-michael.hacer_deposito(100)
-michael.hacer_deposito(200)
-michael.hacer_deposito(50)
-michael.hacer_retiro(45)
-michael.mostrar_balance_usuario()
+michael.hacer_deposito(100).hacer_deposito(200).hacer_deposito(50).hacer_retiro(45).mostrar_balance_usuario()
 
-jenny.hacer_deposito(1000)
-jenny.hacer_deposito(1000)
-jenny.hacer_retiro(500)
-jenny.hacer_retiro(300)
-jenny.mostrar_balance_usuario()
+jenny.hacer_deposito(1000).hacer_deposito(1000).hacer_retiro(500).hacer_retiro(300).mostrar_balance_usuario()
 
-mario.hacer_deposito(1500)
-mario.hacer_retiro(1000)
-mario.hacer_retiro(5000)
-mario.hacer_retiro(3000)
-mario.mostrar_balance_usuario()
+mario.hacer_deposito(1500).hacer_retiro(1000).hacer_retiro(5000).hacer_retiro(3000).mostrar_balance_usuario()
 
-jenny.transferir_dinero(400 ,michael)
+jenny.transferir_dinero(400 ,michael) #bonus
